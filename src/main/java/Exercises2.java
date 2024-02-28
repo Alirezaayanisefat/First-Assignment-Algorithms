075d67a03fd1d1eb12986e9df51548c361bdbcea
Alireza <alirezaayanisefat1383@gmail.com>
2024-02-28 13:28:14 +0330

    lkccjdj

--- src/main/java/Exercises2.java
+++ src/main/java/Exercises2.java
@@ -14,8 +14,23 @@ public class Exercises2 {
     */
 
     public int[] twoSum(int[] nums, int target) {
-        // TODO
-        return null;
+        int temp;
+
+        int [] arr = new int [2];
+        for(int i = 0; i < nums.length - 1; i++)
+        {
+            for(int j = 1; j + i < nums.length; j++)
+            {
+                temp = nums[i] + nums[i + j];
+                if(temp == target)
+                {
+                    arr[0] = i;
+                    arr[1] = i + j;
+                    break;
+                }
+            }
+        }
+        return arr;
     }
 
     /*
@@ -34,7 +49,6 @@ public class Exercises2 {
     12 is written as XII, which is simply X + II.
     The number 27 is written as XXVII, which is XX + V + II.
 
-    Roman numerals are usually written largest to smallest from left to right.
     However, the numeral for four is not IIII.
     Instead, the number four is written as IV.
     Because the one is before the five we subtract it making four.
@@ -48,22 +62,77 @@ public class Exercises2 {
     Given a roman numeral, convert it to an integer.
     */
 
-    public int romanToInt(String s) {
-        // TODO
-        return 0;
-    }
+    public static int romanToInt(String s) {
+        int num = 0;
+        char ch;
+        for(int i = 0; i < s.length() ; i++)
+        {
+            ch = s.charAt(i);
+            switch(ch)
+            {
+                case 'I':
+                    if(s.charAt(i + 1) == 'V') {
+                        num += 4;
+                        i = i + 1;
+                    }
+                    else if(s.charAt(i + 1) == 'X') {
+                        num += 9;
+                        i = i + 1;
+                    }
+                    else
+                        num += 1;
+                    break;
+                case 'V':
+                    num += 5;
+                    break;
+                case 'X':
+                    if(s.charAt(i + 1) == 'L') {
+                        num += 40;
+                        i = i + 1;
+                    }
+                    else if(s.charAt(i + 1) == 'C') {
+                        num += 90;
+                        i = i + 1;
+                    }
+                    else
+                        num += 10;
+                    break;
+                case 'L':
+                    num += 50;
+                    break;
+                case 'C':
+                    if(s.charAt(i + 1) == 'D') {
+                        num += 400;
+                        i = i + 1;
+                    }
+                    else if(s.charAt(i + 1) =='M') {
+                        num += 900;
+                        i = i + 1;
+                    }
+                    else
+                        num += 100;
+                    break;
+                case 'D':
+                    num += 500;
+                    break;
+                case 'M':
+                    num += 1000;
+                    break;
+            }
+        }
 
+        return num;
+    }
     /*
+
     Given an array nums of distinct integers, return all the possible permutations.
     You can return the answer in any order.
     */
 
     public List<List<Integer>> permute(int[] nums) {
-        // TODO
-        return null;
+    return null;
     }
 
     public static void main(String[] args) {
-        // test your code here!
     }
 }
